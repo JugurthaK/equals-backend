49f63e12aa386e69b2f805c41af5542a433d458d
jugurthak <jugurthak@protonmail.com>
2022-03-24 10:40:34 +0100

    startDate attribute added & color handler

--- utils/generator.js
+++ utils/generator.js
@@ -1,15 +1,16 @@
 const { numberFormatter } = require('./typemanager');
+const colors = require('./colors');
 
 const generateDatasets = (data) => {
   const labels = [];
   const headers = Object.keys(data[0]);
   const year = headers[0];
   const headersWithoutYear = headers.slice(1);
-  const datasets = headersWithoutYear.map((header) => ({
+  const datasets = headersWithoutYear.map((header, index) => ({
     label: header,
     data: [],
-    borderColor: '#f28de0',
-    backgroundColor: '#f28de0',
+    borderColor: colors[index % colors.length],
+    backgroundColor: colors[index % colors.length],
     ...(header.indexOf('Hommes & Femmes') > -1 && { hidden: true }),
   }));
 
@@ -19,6 +20,7 @@ const generateDatasets = (data) => {
       const value = d[header];
       const formatted = numberFormatter(value);
       if (isNaN(formatted)) datasets[index].spanGaps = true;
+      if (parseInt(d[year], 10) < 2000) datasets[index].startDate = 2000;
       datasets[index].data.push(formatted);
     });
   });
